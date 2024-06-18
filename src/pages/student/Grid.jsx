8402d3a5992fc8b73a9bc27823c1330812653108
Swapnil Srivastava <swapnilsks123.ss@gmail.com>
2024-06-18 18:06:55 +0530

    done with dashboard

--- src/pages/student/Grid.jsx
+++ src/pages/student/Grid.jsx
@@ -4,21 +4,33 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
-
-export default function ComplexGrid() {
+import AlarmOffIcon from '@mui/icons-material/AlarmOff';
+export default function ComplexGrid(props) {
   return (
     <div
       style={{
-        p: 2,
+
         margin: 'auto',
         minHeight: 100,
         maxWidth: 250,
         flexGrow: 1,
         backgroundColor: "#EEF5FF",
         border: "none",
-        borderRadius: "15px"
+        borderRadius: "15px",
+        display: "flex",
+        justifyContent: "center",
+
       }}
     >
+      <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: 'center' }}>
+          <AlarmOffIcon color="primary" />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", flexDirection: 'column' }}>
+          <div style={{ fontWeight: "bold" }}>{props.title}</div>
+          <div>{props.value}</div>
+        </div>
+      </div>
     </div>
   );
 }
