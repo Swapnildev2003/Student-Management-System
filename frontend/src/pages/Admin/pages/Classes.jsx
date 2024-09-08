3d4f37fc434f47ce52e3230bd15e4233cdab37c5
aditri1001 <adityatripathi1001gmail.com>
2024-09-09 01:31:12 +0530

    modified classes section table

--- frontend/src/pages/Admin/pages/Classes.jsx
+++ frontend/src/pages/Admin/pages/Classes.jsx
@@ -1,93 +1,160 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      {
+        teacher: 'Kumud Patel',
+        Section: 'A',
+        Period: 3,
+        subject: "BEC-101",
+        day: "Monday"
+      },
+      {
+        teacher: 'SP Singh',
+        Section: 'B',
+        Period: 1,
+        subject: "BIT-201",
+        day: "Thursday"
+      },
+    ],
+  };
 }
 
+function Row(props) {
+  const { row } = props;
+  const [backColor, setBackColor] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' }}}>
+        <TableCell component="th" scope="row">{row.name}</TableCell>
+        <TableCell align="center">{row.calories}</TableCell>
+        <TableCell align="center">{row.fat}</TableCell>
+        <TableCell align="center">{row.carbs}</TableCell>
+        <TableCell align="center">{row.protein}</TableCell>
+        <TableCell align="center">
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => {setOpen(!open); setBackColor(!open)}}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6} >
+          <Collapse in={open} timeout="auto" unmountOnExit >
+            <Box sx={{ padding: 1 }} style={{backgroundColor: "#EEEDEB"}}>
+              <Typography variant="h6" gutterBottom component="div">
+                Detail
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell ><b>Teacher Name</b></TableCell>
+                    <TableCell ><b>Subject</b></TableCell>
+                    <TableCell ><b>Section</b></TableCell>
+                    <TableCell align="center"><b>Period</b></TableCell>
+                    <TableCell align="center"><b>Day</b></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody >
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.teacher}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.teacher}
+                      </TableCell>
+                      <TableCell>{historyRow.subject}</TableCell>
+                      <TableCell style={{paddingLeft: 35}}>{historyRow.Section}</TableCell>
+                      <TableCell align="center">{historyRow.Period}</TableCell>
+                      <TableCell align="center">
+                        {historyRow.day}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    calories: PropTypes.number.isRequired,
+    carbs: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    protein: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
 const rows = [
-  createData('Computer Science', 'CSE'	,'1st'	,'A'	,159),
-  createData('Mechanical Engg.',	'ME'	,'2th'	,'B'	,237),
-  createData('Electrical Engg.	','EEE'	,'3th'	,'C'	,262),
-  createData('Civil Engineering',	'CE'	,'4th'	,'D'	,305),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
-  createData('Information Tech',	'IT'	,'1th'	,'E'	,356),
+  createData('BTech', 'Computer Science', 3, '4th', 178, 3.99),
+  createData('BTech', 'Information Technology', 1, '4th', 91, 4.99),
+  createData('MBA', 'MBA', 2, '2nd', 78, 3.79),
+  createData('MCA', 'MCA', 1, '1st', 82, 2.5),
+  createData('BTech', 'Civil', 3, '4th', 89, 1.5),
+  createData('BTech', 'Chemical', 2, '4th', 125, 1.5),
+  createData('MSC', 'MSC', 1, '2nd', 85, 1.5),
+  createData('BTech', 'Electrical', 2, '4th', 131, 1.5),
+  createData('BTech', 'Electronics and Comm.', 3, '4th', 121, 1.5),
 ];
 
-export default function CustomizedTables() {
+export default function CollapsibleTable() {
   return (
-    <>
-
-       <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead >
-          <TableRow >
-            <StyledTableCell style={{backgroundColor: "#1976d2"}}>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right" style={{backgroundColor: "#1976d2"}}>Calories</StyledTableCell>
-            <StyledTableCell align="right" style={{backgroundColor: "#1976d2"}}>Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right" style={{backgroundColor: "#1976d2"}}>Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right" style={{backgroundColor: "#1976d2"}}>Protein&nbsp;(g)</StyledTableCell>
+    // <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell style={{backgroundColor: "#3FA2F6"}}>Courses</TableCell>
+            <TableCell align="center" style={{backgroundColor: "#3FA2F6"}}>Branch</TableCell>
+            <TableCell align="center" style={{backgroundColor: "#3FA2F6"}}>Total Sections</TableCell>
+            <TableCell align="center" style={{backgroundColor: "#3FA2F6"}}>Course Year</TableCell>
+            <TableCell align="center" style={{backgroundColor: "#3FA2F6"}}>Total Student</TableCell>
+            <TableCell align="center" style={{backgroundColor: "#3FA2F6"}}>Details</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name} >
-              <StyledTableCell component="th" scope="row" >
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right" >{row.calories}</StyledTableCell>
-              <StyledTableCell align="right" >{row.fat}</StyledTableCell>
-              <StyledTableCell align="right" >{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right" >{row.protein}</StyledTableCell>
-            </StyledTableRow>
+            <Row key={row.name} row={row} />
           ))}
         </TableBody>
       </Table>
-    </>
+    // </TableContainer>
   );
 }
